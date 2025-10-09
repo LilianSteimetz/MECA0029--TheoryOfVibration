fixed lumped mass, added mass computation, improved generalization

python/MECA0029_Group16_1.py
@@ -5,7 +5,7 @@ import numpy as np
 # eigsh is used like eig but computes a given number of eigenvalues
 from scipy.sparse.linalg import eigsh
 
-
+""" Computation of the 6 1st natural frequencies and mode shapes of the structure """
 M_global, K_global = create_globalMass_and_globalStiffness()
 
 eigvals, eigvecs = eigsh(K_global, k=6, M=M_global, sigma=0.0, which='LM')
@@ -41,7 +41,7 @@ eigvecs = eigvecs_full
 
 
 """Visualization of a mode shape"""
-mode_idx = 4  # mode to visualize
+mode_idx = 3  # mode to visualize
 
 
 U = np.zeros((nNodes, 3))  # x, y, z displacement per node
@@ -51,4 +51,4 @@ for i in range(nNodes):
     U[i, 2] = eigvecs[dofList[i, 2]-1, mode_idx]
 
 
-plot_structure(elemList, nodeList + U*0.5)
+plot_structure(elemList, nodeList + U*0)