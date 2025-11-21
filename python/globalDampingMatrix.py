7596c57439c0e51cc025fe421769d42ac5f8ebdc
LilianSteimetz <150150899+LilianSteimetz@users.noreply.github.com>
2025-11-21 15:01:37 +0100

    part3_damp_mat

--- python/globalDampingMatrix.py
+++ python/globalDampingMatrix.py
@@ -1,6 +1,10 @@
 import numpy as np
 from scipy.sparse.linalg import eigsh
-
+from constants import *
+from globalMassStiffMatrices import create_globalMass_and_globalStiffness
+from mesh import elemList, nodeList, dofList, plot_structure, vector_to_constrained
+from geometry import constrainedNodes
+from matplotlib import pyplot as plt
 
 def create_damping_matrix(M, K, damping1=0.01, damping2=0.01):
     # damping ratios
@@ -19,7 +23,38 @@ def create_damping_matrix(M, K, damping1=0.01, damping2=0.01):
     # with 2 modal damping ratios given
     Omega_mat = np.array([[omega1, 1/omega1], [omega2, 1/omega2]])
     alpha, beta = 2 * np.linalg.solve(Omega_mat, epsilon)
+    print(f"Computed Rayleigh damping coefficients: alpha = {alpha:.6e}, beta = {beta:.6e}")
 
     C = alpha * K + beta * M
 
     return C
+
+M_global, K_global = create_globalMass_and_globalStiffness()
+damp_mat = create_damping_matrix(M_global, K_global)
+eigvals, eigvecs = eigsh(K_global, k=desiredFreqNb,
+                         M=M_global, sigma=0.0, which='LM')
+
+nat_freqs = np.sqrt(eigvals)/(2*np.pi)
+idx = np.argsort(nat_freqs)
+nat_freqs = nat_freqs[idx]
+eigvecs = eigvecs[:, idx]
+eigvecs = eigvecs/np.max(np.abs(eigvecs), axis=0)
+epsilon = np.zeros(desiredFreqNb)
+
+for i in range(6):
+    beta_i = eigvecs[:, i].T @ damp_mat @ eigvecs[:, i]
+    omega_i = np.sqrt(eigvals[i])
+    mu_i =  eigvecs[:, i].T @ M_global@ eigvecs[:, i]
+    epsilon[i] = (beta_i) / (2 * mu_i * omega_i)
+    
+epsilon_2 = np.zeros(desiredFreqNb)
+alpha = 3.380138e-04
+beta = 2.349904e-01
+for i in range(6):
+    epsilon_2[i] = ((alpha * np.sqrt(eigvals[i])) + (beta / np.sqrt(eigvals[i]))) / 2
+    
+plt.plot(np.sqrt(eigvals), epsilon, 'o-')
+plt.show()
+
+print(f"First six damping ratios: {epsilon[:]}")
+print(f"First six damping ratios: {epsilon_2[:]}")
